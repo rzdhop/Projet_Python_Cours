95239e915c345c4f9873ba86039ebaa4d1b75353
IchInose_0x3b <45741890+rzdhop@users.noreply.github.com>
2022-05-16 23:44:50 +0200

    finnalisaion TP2

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -1,18 +1,38 @@
 import pandas as pd 
 import seaborn as sns
 
-df_entite = pd.read_csv("../../assets/full-oldb-20220110/nodes-addresses.csv")
-df_officers = pd.read_csv("../../assets/full-oldb-20220110/nodes-officers.csv")
+#Charge les different offshore leaks, la parametre low_memory=False permet de preciser que les donnees sont grandes
+#et donc de desactiver les warnings
+df_entite = pd.read_csv("../../assets/full-oldb-20220110/nodes-addresses.csv", low_memory=False)
+df_officers = pd.read_csv("../../assets/full-oldb-20220110/nodes-officers.csv", low_memory=False)
 
-print("\n\nExo 1 : \n")
+#Ici je defini une fonction affichant a l'ecrant la reponse au questions du premier set 
+def Exo1() :
+    print("\n\nExo 1 : \n")
+    #la ligne suivante affiche le nombre d'entitie en mesurent le retour de la dataframe avec un filtre sur les 'offshore leaks'
+    print("Nombre d'entites : " + str(len(df_entite[df_entite["sourceID"].str.lower() == "offshore leaks"]["countries"].value_counts())))
+    #Celle-ci mesure le retour du filtre sur 'irland' dans la dataframe
+    print("Irland apparait : " + str(len(df_entite[df_entite["countries"].str.lower() == "ireland"])) + " fois.")
+    #pour fini cette exercice, dans la ligne suivante j'affiche le nombre d'occurence des nom egal a 'el portdor'
+    print("El Portador appait : " + str(len(df_officers[df_officers["name"].str.lower() == "el portador"])) + " fois.")
 
-print("Nombre d'entitÃ©es : " + str(len(df_entite[df_entite["sourceID"].str.lower() == "offshore leaks"]["countries"].value_counts())))
-print("Irland apparait : " + str(len(df_entite[df_entite["countries"].str.lower() == "ireland"])) + " fois.")
-print("El Portador appait : " + str(len(df_officers[df_officers["name"].str.lower() == "el portador"])) + " fois.")
+#Ici je defini une fonction affichant a l'ecrant la reponse au questions du second set
+def Exo2() :
+    print("\n\nExo 2 : \n")
+    #cette ligne va permettre d'afficher un graphiique correlant les occurence de nom d'un pays et sont nom
+    sns.countplot(x="countries", data=df_entite[df_entite["sourceID"].str.lower() == "offshore leaks"])
+    #ici j'affiche les 20 premiere pays avec le plus d'occurence
+    df_entite[df_entite["sourceID"].str.lower() == "offshore leaks"].groupby(["countries"]).size().sort_values(ascending=False)[:20]
 
+#je n'ai malheureusement pas compris la questions
+def Exo3() :
+    pass
 
-print("\n\nExo 2 : \n")
+def main():
+    Exo1() #Execute la fonction contenant l'exercice 1
+    Exo1() #meme chose mais pour l'exercice 2
+    #Exo3()
 
-sns.countplot(x="countries", data=df_entite[df_entite["sourceID"].str.lower() == "offshore leaks"])
+if __name__ == "__main__":
+    main()
 
-df_entite[df_entite["sourceID"].str.lower() == "offshore leaks"].groupby(["countries"]).size().sort_values(ascending=False)[:20]
