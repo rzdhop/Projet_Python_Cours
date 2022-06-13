86faa7c138d3da095a51bfad31a0eff959810505
ichinose <rida.pgm@gmail.com>
2022-06-13 19:57:48 +0200

    add API field to clientlourd

--- TP3/ClientLourd.py
+++ TP3/ClientLourd.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from json import dumps
+import json
 import sys, requests
 
 
@@ -18,6 +18,9 @@ class Window(QWidget):
         self.ipLabel = QLabel(self)
         self.ipLabel.setText("Entrez l'ip a geolocaliser :")
         self.ipEntree = QLineEdit(self)
+        self.APILabel = QLabel(self)
+        self.APILabel.setText("Entrez l'API :")
+        self.APIEntree = QLineEdit(self)
         self.submitButton = QPushButton(self)
         self.submitButton.setText("Envoyer !")
         self.exitButton = QPushButton(self)
@@ -27,9 +30,11 @@ class Window(QWidget):
        
         self.ipLabel.move(10, 10)
         self.ipEntree.move(10, 30)
-        self.submitButton.move(10, 70)
-        self.exitButton.move(120, 70)
-        self.responseLabel.move(10, 100)
+        self.APILabel.move(10, 55)
+        self.APIEntree.move(10, 75)
+        self.submitButton.move(10, 100)
+        self.exitButton.move(120, 100)
+        self.responseLabel.move(10, 140)
 
         self.submitButton.clicked.connect(self.request)
         self.exitButton.clicked.connect(self.echap)
@@ -38,11 +43,13 @@ class Window(QWidget):
 
     def request(self):
         IP = self.ipEntree.text()
+        API = self.APIEntree.text()
         if len(IP) < 7 :
             return
-        response = requests.get(f"http://localhost:8000/ip/{IP}")
-
-        self.responseLabel.insertPlainText(dumps(response.json()))
+        response = requests.get(f"http://localhost:8000/{IP}/{API}")
+        #https://www.openstreetmap.org/#map=18/
+        loclist = [loc["location"].replace(',', '/') for loc in json.loads(response.json())]
+        self.responseLabel.insertPlainText(f"{loc} -> https://www.openstreetmap.org/#map=18/{loc}" for loc in loclist)
 
     def echap(self):
         exit()
--- TP3/TP3_server.py
+++ TP3/TP3_server.py
@@ -1,23 +1,21 @@
-from typing import Optional
 from fastapi import FastAPI
 import requests
-import json
 
 app = FastAPI()
 
 #Défini le chemin de l'API 
-@app.get("/ip/{IP}")
-def read_root(IP):
-    apiKey = "603d5d2937621744d6de5519f2283155a32af272"
+@app.get("/{IP}/{API}")
+def read_root(IP, API):
     header = {
-        "Authorization":f"apiKey {apiKey}",
+        "Authorization":f"apiKey {API}",
         "Content-Type":"application/json"
     }
     r = requests.get(f"https://www.onyphe.io/api/v2/simple/geoloc/{IP}", headers=header)
 
     return r.json()["results"]
 
+
 #uvicorn TP3_server:app --reload
-#login : rida@e-mail.cafe:FzugGiaNs6 API:603d5d2937621744d6de5519f2283155a32af272
+#login : rida@e-mail.cafe:FzugGiaNs6 key:603d5d2937621744d6de5519f2283155a32af272
 #source ../../PythonVenv/bin/activate
 #/mnt/c/Users/ridap/Documents/GitHub/Projet_Python_Cours
